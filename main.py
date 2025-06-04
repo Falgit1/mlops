7a982721d23e63ab022aa4107794e17a69c4ed7b
Falgit1 <140887788+Falgit1@users.noreply.github.com>
2025-06-04 17:53:56 +0530

    Base model preparation module done

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from Plant_class import logger
 from Plant_class.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from Plant_class.pipeline.stage02_prepare_base_model import BaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
     logger.info(f"======== Stage : {STAGE_NAME} started =========")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"======== Stage : {STAGE_NAME} completed =========")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare Base Model Stage"
+
+try:
+    logger.info(f"======== Stage : {STAGE_NAME} started =========")
+    obj = BaseModelTrainingPipeline()
     obj.main()
     logger.info(f"======== Stage : {STAGE_NAME} completed =========")
 except Exception as e:
---
+++ src/Plant_class/pipeline/stage02_prepare_base_model.py
@@ -0,0 +1,28 @@
+from Plant_class.config.configuration import ConfigurationManager
+from Plant_class.components.prepare_base_model import PrepareBaseModel
+from Plant_class import logger
+
+STAGE_NAME = "Prepare Base Model Stage"
+
+
+class BaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"======== Stage : {STAGE_NAME} started =========")
+        obj = BaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f"======== Stage : {STAGE_NAME} completed =========")
+    except Exception as e:
+        logger.exception(e)
+        raise e
